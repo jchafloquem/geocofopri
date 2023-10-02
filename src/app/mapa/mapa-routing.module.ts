3c68324e75648cbc8fb240c0cacc4406b855c811
Jorge Chafloque Millones <jchafloque.gis@gmail.com>
2023-10-02 01:12:47 -0500

    Cargado de mapa Leaflet

--- src/app/mapa/mapa-routing.module.ts
+++ src/app/mapa/mapa-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { PortalComponent } from './pages/portal/portal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children:[
+      {path: 'geoportal', component: PortalComponent },
+      { path: '**', redirectTo: 'geoportal', pathMatch:'full'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/mapa/pages/portal/portal.component.ts
+++ src/app/mapa/pages/portal/portal.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Map, control, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
   styleUrls: ['./portal.component.css']
 })
-export class PortalComponent {
+export class PortalComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new Map('map', {
+      zoomControl: false,
+      minZoom: 6
+    }
+    ).setView([-9.1963858, -75.3050354], 6);
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    //Capas Base
 
+    //Capas Cartograficas
+    //Instituo Geofisico del Peru
+    const igndepartamentos = tileLayer.wms("https://www.idep.gob.pe/geoportal/services/DATOS_GEOESPACIALES/L%C3%8DMITES/MapServer/WMSServer", {
+      layers: '0',
+      format: 'image/png',
+      transparent: true,
+    }).addTo(map);
+    const ignprovincia = tileLayer.wms("https://www.idep.gob.pe/geoportal/services/DATOS_GEOESPACIALES/L%C3%8DMITES/MapServer/WMSServer", {
+      layers: '2',
+      format: 'image/png',
+      transparent: true,
+    });
+    const igndistrito = tileLayer.wms("https://www.idep.gob.pe/geoportal/services/DATOS_GEOESPACIALES/L%C3%8DMITES/MapServer/WMSServer", {
+      layers: '3',
+      format: 'image/png',
+      transparent: true,
+    });
+    //Autoridad Nacional del Agua
+    const anarios = tileLayer.wms("https://geosnirh.ana.gob.pe/server/services/Público/Rios/MapServer/WMSServer", {
+      layers: '0',
+      format: 'image/png',
+      transparent: true,
+    });
+    const analagunas = tileLayer.wms("https://geosnirh.ana.gob.pe/server/services/Público/Lagunas/MapServer/WMSServer", {
+      layers: '0',
+      format: 'image/png',
+      transparent: true,
+    }).addTo(map);
+    const cofopri = tileLayer.wms('http://172.16.16.33:8081/geoserver/GeoCatastro/wms?', {
+      layers: 'tg_cartografia',
+      format: 'image/png',
+      maxZoom: 22,
+      transparent: true
+    });
+  }
 }
