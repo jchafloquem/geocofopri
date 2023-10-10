c1afd841c085f189a40c3a800ec08ad4821dffac
Arnold Ramos Calderon <aramosc.16@hotmail.com>
2023-10-10 00:59:59 -0500

    refactorizacion de fab-container-botton

--- src/app/mapa/pages/portal/components/fab-container-botton/fab-container-botton.component.ts
+++ src/app/mapa/pages/portal/components/fab-container-botton/fab-container-botton.component.ts
@@ -1,6 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {PortalService} from '../../service/portal.service';
 
+export interface data {
+	icon: string;
+	descripcion: string;
+	f?: () => void;
+	childrenView: boolean;
+	children?: data[];
+}
+
 @Component({
 	selector: 'app-fab-container-botton',
 	templateUrl: './fab-container-botton.component.html',
@@ -8,4 +16,68 @@ import {PortalService} from '../../service/portal.service';
 })
 export class FabContainerBottonComponent {
 	public _portalService = inject(PortalService);
+	mapaBaseView = false;
+	mapaBaseView2 = false;
+	data: data[] = [
+		{
+			icon: '1',
+			descripcion: '',
+			f: () => this._portalService.mapabase('Callejero'),
+			childrenView: false,
+			children: [
+				{
+					icon: '1',
+					descripcion: '',
+					f: () => this._portalService.mapabase('Callejero'),
+					childrenView: false,
+				},
+			],
+		},
+		{
+			icon: '2',
+			descripcion: '',
+			childrenView: false,
+			children: [
+				{
+					icon: '1',
+					descripcion: '',
+					f: () => this._portalService.mapabase('Topográfico'),
+					childrenView: false,
+				},
+				{
+					icon: '2',
+					descripcion: '',
+					f: () => this._portalService.mapabase('Topográfico'),
+					childrenView: false,
+				},
+			],
+		},
+		{
+			icon: '3',
+			descripcion: '',
+			f: () => this._portalService.mapabase('Topográfico'),
+			childrenView: false,
+
+			children: [
+				{
+					icon: '1',
+					descripcion: '',
+					f: () => this._portalService.mapabase('Topográfico'),
+					childrenView: false,
+				},
+				{
+					icon: '2',
+					descripcion: '',
+					f: () => this._portalService.mapabase('Topográfico'),
+					childrenView: false,
+				},
+				{
+					icon: '1',
+					descripcion: '',
+					f: () => this._portalService.mapabase('Topográfico'),
+					childrenView: false,
+				},
+			],
+		},
+	];
 }
